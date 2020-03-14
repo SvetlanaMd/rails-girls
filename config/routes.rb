b62f1e68edb510b0375c1146b5d108e7045c7547
Svetlana <s.medelian@gmail.com>
2020-03-14 15:15:42 +0000

    3rd commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'pages/info'
+  root to: redirect('/ideas')
+  get "pages/info"
   resources :ideas
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
